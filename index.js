3754321a4d7a71bfd59c8d99966b6e9525191bb1
kazmi017 <kazmi82033@gmail.com>
2022-05-27 08:48:34 +0500

    done

--- index.js
+++ index.js
@@ -1,31 +1,99 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import multer from 'multer'
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+import e from 'express';
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
-
-mongoose.connect("mongodb://localhost:27017/smartTravellingDB" ,{
+const connection = "mongodb://localhost:27017/smartTravellingDB"
+// const connection = "mongodb+srv://fyp2022:ptdmptdm5124@cluster0.sbe6i.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect( connection ,{
     useNewUrlParser:true,
     useUnifiedTopology:true
-},()=>{
-    console.log("DB connected")
-})
+}).then(() => console.log("Database Connected Successfully"))
+.catch(err => console.log(err));
 
 const userSchema = new mongoose.Schema({
     fName:String,
     lName:String,
     email:String,
-    dob:Date,
+    dob:String,
     password:String
+
+})
+const notifSchema = new mongoose.Schema({
+    name:String,
+    description:String
+
+})
+const tripSchema = new mongoose.Schema({
+    name:String,
+    description:String,
+    place:String,
+    date:String,
+    seats:String,
+    src:String
+
+})
+const tripBookSchema = new mongoose.Schema({
+    email:String,
+    name:String,
+    place:String,
+    date:String,
+    seats:String
+
+})
+const hotelSchema = new mongoose.Schema({
+    name:String,
+    rent:String,
+    place:String,
+    star:String,
+
+})
+const hotelBookSchema = new mongoose.Schema({
+    email:String,
+    location:String,
+    rent:String,
+    ina:String,
+    out:String,
+    rooms:String,
+    type:String
+
+})
+const carSchema = new mongoose.Schema({
+    name:String,
+    engine:String,
+    rent:String,
+
+})
+const carBookSchema = new mongoose.Schema({
+    orgin:String,
+    destination:String,
+    days:String,
+    email:String,
+    name:String,
+    engine:String,
+    rent:String,
 
 })
 
 const User = new mongoose.model('User', userSchema)
+const Car = new mongoose.model('Car', carSchema)
+const Trip = new mongoose.model('Trip', tripSchema)
+const Hotel = new mongoose.model('Hotel', hotelSchema)
+const Notif = new mongoose.model('Notification', notifSchema)
+
+const CarB = new mongoose.model('CarBook', carBookSchema)
+const TripB = new mongoose.model('TripBook', tripBookSchema)
+const HotelB = new mongoose.model('HotelBook', hotelBookSchema)
 
 app.get('/home', (req, res) => res.send('Hello world!'));
+
 
 // Routes
 app.post("/SignIn", (req , res )=>{
@@ -45,8 +113,36 @@ app.post("/SignIn", (req , res )=>{
 })
 })
 
+const storage= multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'./images');
+    },
+    filename: function(req,file,cb){
+        cb(null,uuidv4()+'-'+Date.now()+path.extname(file.originalname))
+    }
+});
+
+const fileFilter=(req,file,cb)=>{
+    const allowedFileTypes=['images/jpeg','images/jpg','images/png'];
+    if(allowedFileTypes.includes(file.mimetype)){
+        cb(null,true);
+    }
+    else{
+        cb(null,false);
+    }
+}
+
+const upload=multer({storage,fileFilter});
+
 app.post("/SignUp", (req , res )=>{
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
     const {fName ,lName , email, dob , password} = req.body
+    console.log(req.body)
     User.findOne({email:email},(err,user)=>{
         if(user){
             res.send({message:"User already Registered"})
@@ -57,7 +153,7 @@ app.post("/SignUp", (req , res )=>{
                 lName,
                 email,
                 dob,
-                password 
+                password
             })
             user.save( err =>{
                 if(err){
@@ -71,21 +167,341 @@ app.post("/SignUp", (req , res )=>{
             })
         }
     })
+}
     
 })
-const port = process.env.PORT || 5000;
-app.listen(port,()=>{
-    console.log("BE started at port 9002");
+// TRIP
+app.get("/Trip",(req,res)=>{
+    Trip.find().then(function(results){
+        res.send(results);
+    }).catch(function(err){
+        console.error(err);
+    });
 })
 
-// app.js
+app.post("/Trip",(req,res)=>{
+    
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
+        const {name,description,place,date,seats,src} = req.body 
+        console.log(req.body)
+    const trip = new Trip({
+        name,
+        description,
+        place,
+        date,
+        seats,
+        src
+    })
+    trip.save( err =>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send({
+                message : "Successfully Created"
+            })
+        }
+    })
+    }
 
-// // const express = require('express');
 
-// const app = express();
+})
+// Notification
+app.get("/Notif",(req,res)=>{
+    Notif.find().then(function(results){
+        res.send(results);
+    }).catch(function(err){
+        console.error(err);
+    });
+})
 
-// app.get('/', (req, res) => res.send('Hello world!'));
+app.post("/Notif",(req,res)=>{
+    
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
+        const {name,description} = req.body 
+        console.log(req.body)
+    const notif = new Notif({
+        name,
+        description
+    })
+    notif.save( err =>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send({
+                message : "Successfully Created"
+            })
+        }
+    })
+    }
 
-// const port = process.env.PORT || 8082;
 
-// app.listen(port, () => console.log(`Server running on port ${port}`));
+})
+//Car
+app.get("/Car",(req,res)=>{
+    Car.find().then(function(results){
+        res.send(results);
+    }).catch(function(err){
+        console.error(err);
+    });
+})
+
+app.post("/Car",(req,res)=>{
+    
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
+        const {name,engine,rent} = req.body 
+        console.log(req.body)
+    const car = new Car({
+        name,
+        engine,
+        rent
+    })
+    car.save( err =>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send({
+                message : "Successfully Created"
+            })
+        }
+    })
+    }
+
+
+})
+
+//Hotel
+app.get("/Hotel",(req,res)=>{
+    Hotel.find().then(function(results){
+        res.send(results);
+    }).catch(function(err){
+        console.error(err);
+    });
+})
+
+app.post("/Hotel",(req,res)=>{
+    
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
+        const {name,rent,place,star} = req.body 
+        console.log(req.body)
+    const hotel = new Hotel({
+        name,
+        rent,
+        place,
+        star
+    })
+    hotel.save( err =>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send({
+                message : "Successfully Created"
+            })
+        }
+    })
+    }
+
+
+})
+
+//User
+app.get("/User",(req,res)=>{
+    User.find().then(function(results){
+        res.send(results);
+    }).catch(function(err){
+        console.error(err);
+    });
+})
+
+//User Details
+app.get("/UserDs",(req,res)=>{
+
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
+        const { email } = req.body
+        User.findOne({email:email},(err,user)=>{
+            if(user){
+                res.send(user);
+            }
+            else{
+                res.send({message:"User not Found"})
+            }
+    })
+
+      }
+})
+
+//Bookings
+app.post("/TripBook",(req,res)=>{
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
+        const { email,
+            name,
+            place,
+            date,
+            seats} = req.body
+            const tB=new TripB({ 
+                email,
+                name,
+                place,
+                date,
+                seats} )
+            tB.save(err =>{
+                if(err){
+                    res.send(err)
+                }
+                else{
+                    res.send({
+                        message : "Successfully Created"
+                    })
+            }
+        })
+
+      }
+})
+app.get("/TripBook",(req,res)=>{
+    TripB.find().then(function(results){
+        res.send(results);
+    }).catch(function(err){
+        console.error(err);
+    });
+})
+
+app.post("/CarBook",(req,res)=>{
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
+        const { 
+            origin,
+            destination,
+            days,
+            email,
+            name,
+            engine,
+            rent,
+            
+        } = req.body
+            const cB=new CarB(
+                { 
+                    origin,
+                    destination,
+                    days,
+                    email,
+                    name,
+                    engine,
+                    rent,
+                    
+                }
+             )
+            cB.save(err =>{
+                if(err){
+                    res.send(err)
+                }
+                else{
+                    res.send({
+                        message : "Successfully Created"
+                    })
+            }
+        })
+
+      }
+})
+
+app.get("/CarBook",(req,res)=>{
+    CarB.find().then(function(results){
+        res.send(results);
+    }).catch(function(err){
+        console.error(err);
+    });
+})
+
+app.post("/HotelBook",(req,res)=>{
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        res.send({
+            message:"Data Required"
+        })
+      }else{
+        const { 
+            email,
+            location,
+            rent,
+            ina,
+            out,
+            rooms,
+            type
+            
+        } = req.body
+            const hB=new HotelB(
+                { 
+                    email,
+                    location,
+                    rent,
+                    ina,
+                    out,
+                    rooms,
+                    type
+                    
+                }
+             )
+            hB.save(err =>{
+                if(err){
+                    res.send(err)
+                }
+                else{
+                    res.send({
+                        message : "Successfully Created"
+                    })
+            }
+        })
+
+      }
+})
+
+app.get("/HotelBook",(req,res)=>{
+    HotelB.find().then(function(results){
+        res.send(results);
+    }).catch(function(err){
+        console.error(err);
+    });
+})
+
+
+const port = process.env.PORT || 5002;
+app.listen(port,()=>{
+    console.log("BE started at port ",port);
+})
